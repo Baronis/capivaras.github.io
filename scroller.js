ea2484611d83f3685c267cabb1c039bcfcbf1cbc
Haskell Camargo <marcelocamargo@linuxmail.org>
2015-04-08 23:10:08 -0300

    BG

--- scroller.js
+++ scroller.js
@@ -1,17 +1,17 @@
 (function() {
+  window.fixedVisible = false;
   document.body.onscroll = function(e) {
-    var fixedVisible = false;
     if (window.scrollY >= 160) {
       // Show fixed bar
       if (!fixedVisible) {
         console.log("Show");
-        fixedVisible = true;
+        window.fixedVisible = true;
       }
     } else {
       // Hide fixed bar
       if (fixedVisible) {
         console.log("Hide");
-        fixedVisible = false;
+        window.fixedVisible = false;
       }
     }
   };
