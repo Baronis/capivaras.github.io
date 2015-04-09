07491b6e8a147ad289a4c22d721208ff017c5e71
Haskell Camargo <marcelocamargo@linuxmail.org>
2015-04-09 00:06:19 -0300

    CSS

--- scroller.js
+++ scroller.js
@@ -19,7 +19,6 @@
   };
 })();
 
-
 window.fadeOut = function() {
   window.fade(100, 0);
 }
@@ -29,25 +28,13 @@ window.fadeIn = function() {
 }
 
 window.fade = function(ini, fin) {
-  var alpha = ini;
-  var inc;
-  if (fin >= ini) { 
-    inc = 2; 
-  } else {
-    inc = -2;
-  }
-  timer = (0.2 * 1000) / 50;
-  var i = setInterval(
-    function() {
-      if ((inc > 0 && alpha >= fin) || (inc < 0 && alpha <= fin)) {
-        clearInterval(i);
-      }
-    setAlpha(window.fixedHeader, alpha);
-    alpha += inc;
-  }, timer);
-}
-
-function setAlpha(target, alpha) {
+  var alpha = ini, inc = fin >= ini ? 2 : -2;
+  var timer = setInterval(function() {
+    if ((inc > 0 && alpha >= fin) || (inc < 0 && alpha <= fin)) {
+      clearInterval(timer);
+    }
     window.fixedHeader.style.filter = "alpha(opacity="+ alpha +")";
     window.fixedHeader.style.opacity = alpha/100;
+    alpha += inc;
+  }, 40);
 }
