4e685a7ac656acf941afd35cad5c7e6b13059a9e
Haskell Camargo <marcelocamargo@linuxmail.org>
2015-04-08 23:45:50 -0300

    CSS

--- scroller.js
+++ scroller.js
@@ -1,10 +1,12 @@
 (function() {
   window.fixedVisible = false;
+  window.header = document.getElementById("header");
+  window.fixedHeader = document.getElementById("fixed_header");
   document.body.onscroll = function(e) {
     if (window.scrollY >= 160) {
       // Show fixed bar
       if (!fixedVisible) {
-        console.log("Show");
+        window.fixedHeader.style.display = "block";
         window.fixedVisible = true;
       }
     } else {
