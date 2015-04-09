db1998a1635bfcabfbe3c32b8d96016d0dcd886b
Haskell Camargo <marcelocamargo@linuxmail.org>
2015-04-09 00:27:51 -0300

    CSS

--- scroller.js
+++ scroller.js
@@ -2,15 +2,15 @@
   window.fixedVisible = false;
   window.fixedHeader = document.getElementById("fixed_header");
   // Fade functions
-  window.fadeOut = function(time) {
-    window.fade(100, 0, time);
+  window.fadeOut = function() {
+    window.fade(100, 0);
   }
 
-  window.fadeIn = function(time) {
-    window.fade(0, 100, time);
+  window.fadeIn = function() {
+    window.fade(0, 100);
   }
 
-  window.fade = function(ini, fin, time) {
+  window.fade = function(ini, fin) {
     var alpha = ini, inc = fin >= ini ? 2 : -2;
     var i = setInterval(function() {
       if ((inc > 0 && alpha >= fin) || (inc < 0 && alpha <= fin)) {
@@ -19,9 +19,9 @@
       window.fixedHeader.style.filter = "alpha(opacity=" + alpha + ")";
       window.fixedHeader.style.opacity = alpha / 100;
       alpha += inc;
-    }, time ? time : 1);
+    }, 1);
   };
-  window.fadeOut(0);
+  window.fadeOut();
   window.fixedHeader.style.webkitTransitionDuration = "0.5s";
   document.body.onscroll = function(e) {
     if (window.scrollY >= 160) {
